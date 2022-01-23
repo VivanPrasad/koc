ba5c7d2fbd2926c13d494d632666a3bb83862407
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2022-01-23 16:35:48 +0000

    Add files via upload

---
+++ Assets/World/Bed.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+const house_id = [0,1,2,3,4]
+
+func _on_Area2D_area_entered(_area):
+	pass
+func _on_Area2D_area_exited(_area):
+	pass
---
+++ Assets/World/Chest.gd
@@ -0,0 +1,10 @@
+extends StaticBody2D
+
+
+
+func _on_Area2D_area_entered(_area):
+	pass # Replace with function body.
+
+
+func _on_Area2D_area_exited(_area):
+	pass # Replace with function body.
