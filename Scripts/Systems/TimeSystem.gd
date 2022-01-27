64d1f6949adf96f18bcd3b83f344f19c32e7734d
MilkyGee <milkcreame@gmail.com>
2022-01-27 13:50:37 -0800

    Time System update and DayInfo UI
    update stuffs

--- Scripts/Systems/TimeSystem.gd
+++ Scripts/Systems/TimeSystem.gd
@@ -13,11 +13,6 @@ export (int) var speed = 192
 #s300 1 irl minute : 5 in-game hours
 #s192 daytime
 #s480 nighttime
-#10pm-2am Night (4 hours)
-#2-6am Early Morning (4 hours)
-#6am-12pm Morning (6 hours)
-#12pm-6pm Noon (6 hours)
-#6pm-10pm Evening (4 hours)
 
 var processed = false
 
@@ -41,20 +36,19 @@ func _process(delta):
 		start_new_day()
 	if hour > 0:
 		processed = false
-	if hour > 7 and hour < 22:
-		speed = 203 #192 #day
-		if hour < 12:
-			time_cycle = "Morning"
-		elif hour > 11 and hour < 18:
-			time_cycle = "Afternoon"
-		elif hour > 17:
-			time_cycle = "Evening"
-	else: #elif hour > 21 or hour < 6:
-		speed = 508 #480 #night
-		if hour > 3 and hour < 8:
+	if hour > 3 and hour < 22:
+		speed = 320*2 #day
+		if hour < 8:
 			time_cycle = "Early Morning"
-		elif hour > 21 and hour < 25 or hour < 4:
-			time_cycle = "Night"
+		elif hour < 12:
+			time_cycle = "Morning"
+		elif hour < 18:
+			time_cycle = "Afternoon"
+		else:
+			time_cycle = "Evening"
+	else:
+		speed = 480*2 #night
+		time_cycle = "Night"
 	
 	if time_cycle == "Morning" and processed == false and TownStats.can_steal:
 		TownStats.open_town()
@@ -79,20 +73,24 @@ func update_labels():
 	$Gold/HBoxContainer/Amount.text = str(PlayerStats.gold)
 	$Margin/Box/Day.text = "Day " + str(TownStats.day)
 	if hour < 13:
-		$Margin/Box/Time.text = str(hour) + ":" + str(minute_interval) + " " + splitter
+		$Margin/Box/Time.text = str(hour) + ":00" + " " + splitter
 	elif hour == 0:
-		$Margin/Box/Time.text = str(12) + ":" + str(minute_interval) + " " + splitter
+		$Margin/Box/Time.text = str(12) + ":00" + " " + splitter
 	else:
-		$Margin/Box/Time.text = str(hour-12) + ":" + str(minute_interval) + " " + splitter
+		$Margin/Box/Time.text = str(hour-12) + ":00" + " " + splitter
 	
-	$Margin/Box/Cycle.text = str(time_cycle)
-	if str(time_cycle) == "Early Morning" or "Evening":
+	$Margin/Box/Cycle.text = time_cycle
+	if time_cycle == "Early Morning":
 		$Margin/Box/Cycle.add_color_override("font_color", Color("ffae70"))
-	elif str(time_cycle) == "Morning":
+		print("a")
+	elif time_cycle == "Morning":
 		$Margin/Box/Cycle.add_color_override("font_color", Color("ffd885"))
-	elif str(time_cycle) == "Afternoon":
-		$Margin/Box/Cycle.add_color_override("font_color", Color("aaaaaa"))
-	elif str(time_cycle) == "Night":
+		print("o")
+	elif time_cycle == "Afternoon":
+		$Margin/Box/Cycle.add_color_override("font_color", Color("cccccc"))
+	elif time_cycle == "Evening":
+		$Margin/Box/Cycle.add_color_override("font_color", Color("ffae70"))
+	elif time_cycle == "Night":
 		$Margin/Box/Cycle.add_color_override("font_color", Color("9789ff"))
 
 func start_new_day():
@@ -113,12 +111,8 @@ func configure_day():
 #int(floor(delta * speed)) = 2
 	#2 * 60-75 = 120-150 in a second
 	#_process runs 120-150 times in a second depending on FPS
+	#10pm -> 4am nighttime (6 hours in 45 seconds, speed = 480)
+	#4am -> 10pm daytime (18 hours in 3 minutes, speed = 270)
 	
-	#300 in-game seconds for every realtime second
-	#5 in-game minutes for every realtime second	
-	#5 in-game hours for every realtime minute
-	#therefore, 5 hour = 1 minute
-	#10pm -> 6am nighttime (8 hours in 1 minute, speed = 480)
-	#6am -> 10pm daytime (16 hours in 5 minutes, speed = 192)
-
+	#formula: speed = (in-game hour * 60) / irl minutes
 var inventory = preload("res://Scenes/UI/InventoryUI.tscn")
